66ceb0c19890e462c087d229007d1c7733cf60de
Luke Gordon <gordysc@gmail.com>
2023-12-01 07:32:50 -0600

    chore: code clean up

--- 2023/2023-12-01/2023-12-01/Program.cs
+++ 2023/2023-12-01/2023-12-01/Program.cs
@@ -1,13 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var digitizer = new Digitizer();
 var lines = File.ReadAllLines(@"../../../Input/File1.txt");
 var total = 0;
 
 foreach (var line in lines) {
-    var first = digitizer.FindFirstDigit(line);
-    var last = digitizer.FindLastDigit(line);
-    var value = Int32.Parse($"{first}{last}");
+    var first = Digitizer.FindFirstDigit(line);
+    var last = Digitizer.FindLastDigit(line);
+    var value = int.Parse($"{first}{last}");
 
     total += value;
 }
@@ -17,7 +16,7 @@ Console.WriteLine(total);
 internal sealed class Digitizer {
     static readonly string[] HumanizedDigits = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-    public int FindFirstDigit(string line) {
+    public static int FindFirstDigit(string line) {
         var digit = line.First(char.IsDigit);
         var digitIdx = line.IndexOf(digit);
 
@@ -31,7 +30,7 @@ internal sealed class Digitizer {
         return int.Parse(digit.ToString());
     }
 
-    public int FindLastDigit(string line) {
+    public static int FindLastDigit(string line) {
         var digit = line.Last(char.IsDigit);
         var digitIdx = line.LastIndexOf(digit);
 
@@ -45,14 +44,14 @@ internal sealed class Digitizer {
         return int.Parse(digit.ToString());
     }
 
-    private string? FindFirstHumanizedDigit(string segment) {
+    private static string? FindFirstHumanizedDigit(string segment) {
         var results = HumanizedDigits.Select(t => (t, segment.IndexOf(t, StringComparison.Ordinal)));
         var result = results.Where(t => t.Item2 > -1).OrderBy(t => t.Item2).FirstOrDefault();
 
         return result == default ? null : result.Item1;
     }
 
-    private string? FindLastHumanizedDigit(string segment) {
+    private static string? FindLastHumanizedDigit(string segment) {
         var results = HumanizedDigits.Select(t => (t, segment.LastIndexOf(t, StringComparison.Ordinal)));
         var result = results.Where(t => t.Item2 > -1).OrderByDescending(t => t.Item2).FirstOrDefault();
 
