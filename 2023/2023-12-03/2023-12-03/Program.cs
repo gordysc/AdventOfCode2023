87a53480e9e161c741f3683c6bb4ff1a1e9da5e2
Luke Gordon <gordysc@gmail.com>
2023-12-03 08:25:45 -0600

    2023-12-03: part 2

--- 2023/2023-12-03/2023-12-03/Program.cs
+++ 2023/2023-12-03/2023-12-03/Program.cs
@@ -2,42 +2,75 @@
 
 using System.Text.RegularExpressions;
 
+var sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
 var lines = File.ReadAllLines(@"../../../Input/File1.txt");
 var total = 0;
+
+var gears = new Dictionary<Point, List<int>>();
+
+for (var loop = 0; loop < lines.Length; loop++)
+{
+    var line = lines[loop];
+    var matches = Regex.Matches(line, @"\*").ToArray();
+    
+    foreach (var match in matches)
+    {
+        gears.Add(new Point(loop, match.Index), new List<int>());
+    }
+}
+
+var evaluator = new Evaluator(gears);
 
 for (var loop = 0; loop < lines.Length; loop++)
 {
     var previous = loop > 0 ? lines[loop - 1] : "";
     var current = lines[loop];
     var next = loop + 1 < lines.Length ? lines[loop + 1] : "";
+    
+    evaluator.Evaluate(loop, previous, current, next);
 
-    var evaluator = new Evaluator(previous, current, next);
-    var calculation = evaluator.Evaluate();
-
-    total += calculation;
 }
 
-Console.WriteLine($"Total: {total}");
-
-internal sealed class Evaluator(string previous, string current, string next)
+foreach (var kvp in gears)
 {
-    public int Evaluate()
+    if (kvp.Value.Count == 2)
+        total += kvp.Value[0] * kvp.Value[1];
+}
+
+sw.Stop();
+
+Console.WriteLine($"Total: {total}");
+Console.WriteLine($"Total Time: {sw.ElapsedMilliseconds}ms");
+
+internal sealed class Evaluator(Dictionary<Point, List<int>> gears)
+{
+    public void Evaluate(int row, string previous, string current, string next)
     {
-        var indices = FindIndices(previous)
-            .Concat(FindIndices(current))
-            .Concat(FindIndices(next))
-            .ToHashSet()
-            .ToArray();
-
         var matches = Regex.Matches(current, @"\d+").ToArray();
-        
-        return matches.Where(match =>
-        {
-            var min = match.Index > 0 ? match.Index - 1 : 0;
-            var max = match.Value.Length + match.Index;
 
-            return indices.Any(idx => idx >= min && idx <= max);
-        }).Select(m => int.Parse(m.Value)).ToArray().Sum();
+        foreach (var match in matches)
+        {
+            AddMatchIfValid(row - 1, match, previous);
+            AddMatchIfValid(row, match, current);
+            AddMatchIfValid(row + 1, match, next);
+        }
+    }
+
+    private void AddMatchIfValid(int row, Match match, string text)
+    {
+        var min = match.Index > 0 ? match.Index - 1 : 0;
+        var max = match.Value.Length + match.Index;
+        var value = int.Parse(match.Value);
+        
+        var indexes = FindIndices(text);
+        foreach (var index in indexes.Where(idx => idx >= min && idx <= max).ToArray())
+        {
+            var point = new Point(row , index);
+            if (gears[point].Count < 2)
+                gears[point].Add(value);
+        }
     }
     
     private int[] FindIndices(string text)
@@ -45,9 +78,11 @@ internal sealed class Evaluator(string previous, string current, string next)
         var indices = new List<int>();
         
         for (var loop = 0; loop < text.Length; loop++)
-            if (!char.IsDigit(text[loop]) && text[loop] != '.')
+            if (text[loop] == '*')
                 indices.Add(loop);
 
         return indices.ToArray();
     }
 }
+
+internal record Point(int Row, int Column);
