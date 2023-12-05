fb387a0234d4d5403afdd5b3660d4319bfc26b5f
Luke Gordon <gordysc@gmail.com>
2023-12-05 09:11:13 -0600

    2023-12-05: part 2

--- 2023/2023-12-05/Program.cs
+++ 2023/2023-12-05/Program.cs
@@ -1,55 +1,97 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Concurrent;
+
 var sw = new System.Diagnostics.Stopwatch();
 
 sw.Start();
+
 var lines = File.ReadAllLines(@"../../../Input/File1.txt");
+var mappers = AdventUtils.CreateMappers(lines);
+var seeds = AdventUtils.CreateSeeds(lines[0]);
+var results = new long[seeds.Length];
 
-var seeds = lines[0].Split(":")[1].Trim().Split(" ").Select(Int64.Parse).ToArray();
-var mappings = (long[]) seeds.Clone();
-var mapped = Enumerable.Repeat(false, mappings.Length).ToArray();
-
-for (var loop = 2; loop < lines.Length; loop++)
+Parallel.ForEach(seeds, (seed, state, index) =>
 {
-    var line = lines[loop];
-    
-    if (string.IsNullOrWhiteSpace(line)) continue;
+    results[index] = seed;
+    foreach (var mapper in mappers)
+        results[index] = mapper.Map(results[index]);
+});
 
-    if (line.Contains("map"))
-    {
-        mapped = Enumerable.Repeat(false, mappings.Length).ToArray();
-        continue;
-    }
+var closest = results.Min();
 
-    var values = line.Split(" ").Select(Int64.Parse).ToArray();
-    var map = new SeedMap(values[0], values[1], values[2]);
-    
-    for (var i = 0; i < mappings.Length; i++)
-    {
-        if (mapped[i]) continue;
-        if (map.InRange(mappings[i]))
-        {
-            mapped[i] = true;
-            mappings[i] = map.Map(mappings[i]);
-        }
-    }
-}
-
-var closest = mappings.Min();
 sw.Stop();
 
-Console.WriteLine($"Result: {mappings.Min()}");
-Console.WriteLine($"Elapsed: {sw.Elapsed.TotalMilliseconds}ms");
+Console.WriteLine($"Closest: {closest}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
 
-internal sealed class SeedMap(long Destination, long Start, long Range)
+internal class AdventUtils
 {
-    public long Map(long source)
+    public static long[] CreateSeeds(string line)
     {
-        return Destination + (source - Start);
+        var entries = line.Split(":")[1].Trim().Split(" ").Select(long.Parse).ToArray();
+        var pairs = entries.Length / 2;
+        var seeds = new List<long>();
+
+        for (var loop = 0; loop < pairs; loop++)
+        {
+            var start = entries[loop * 2];
+            var length = entries[loop * 2 + 1];
+
+            for (var iter = 0; iter < length; iter++)
+                seeds.Add(start + iter);
+        }
+
+        return seeds.ToArray();
     }
-    
-    public bool InRange(long source)
+
+    public static List<Mapper> CreateMappers(string[] lines)
     {
-        return source >= Start && source < Start + Range;
+        var mappers = new List<Mapper>();
+        var mapper = new Mapper();
+
+        for (var loop = 3; loop < lines.Length; loop++)
+        {
+            var line = lines[loop];
+
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+            if (line.Contains("map"))
+            {
+                mappers.Add(mapper);
+                mapper = new Mapper();
+                continue;
+            }
+
+            mapper.AddMap(line);
+        }
+
+        mappers.Add(mapper);
+
+        return mappers;
     }
 }
+
+internal class Mapper
+{
+    private readonly List<SeedMap> _maps = new ();
+
+    public void AddMap(string line)
+    {
+        var values = line.Trim().Split(" ").Select(long.Parse).ToArray();
+        var map = new SeedMap(values[0], values[1], values[2]);
+
+        _maps.Add(map);
+    }
+
+    public long Map(long source)
+    {
+        foreach (var map in _maps)
+            if (source >= map.Start && source < map.Start + map.Range)
+                return map.Destination + (source - map.Start);
+
+        return source;
+    }
+}
+internal record SeedMap(long Destination, long Start, long Range);
