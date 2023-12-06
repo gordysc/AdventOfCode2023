e2ab8ed7bea1fe57b5587bae1b8181edf9317695
Luke Gordon <gordysc@gmail.com>
2023-12-06 04:12:20 -0600

    2023-12-06: part 1

--- 2023/2023-12-05/Program.cs
+++ 2023/2023-12-05/Program.cs
@@ -3,53 +3,63 @@
 var sw = new System.Diagnostics.Stopwatch();
 
 sw.Start();
-var lines = File.ReadAllLines(@"../../../Input/File1.txt");
 
-var seeds = lines[0].Split(":")[1].Trim().Split(" ").Select(Int64.Parse).ToArray();
-var mappings = (long[]) seeds.Clone();
-var mapped = Enumerable.Repeat(false, mappings.Length).ToArray();
+var lines = File.ReadAllLines(@"../../../Input/Example.txt");
+var numbers = lines[0].Split(":")[1].Trim().Split(" ").Select(long.Parse).ToArray();
+var ranges = new List<SeedRange>();
+
+for (var loop = 0; loop < numbers.Length / 2; loop++)
+{
+    var start = numbers[loop * 2];
+    var length = numbers[loop * 2 + 1];
+    
+    ranges.Add(new SeedRange(start, start + length - 1));
+}
+
+var mappers = new List<SeedMapper>();
+var mapper = new SeedMapper();
 
 for (var loop = 2; loop < lines.Length; loop++)
 {
     var line = lines[loop];
     
     if (string.IsNullOrWhiteSpace(line)) continue;
-
+    
     if (line.Contains("map"))
     {
-        mapped = Enumerable.Repeat(false, mappings.Length).ToArray();
+        mappers.Add(mapper);
+        mapper = new SeedMapper();
         continue;
     }
-
-    var values = line.Split(" ").Select(Int64.Parse).ToArray();
-    var map = new SeedMap(values[0], values[1], values[2]);
     
-    for (var i = 0; i < mappings.Length; i++)
-    {
-        if (mapped[i]) continue;
-        if (map.InRange(mappings[i]))
-        {
-            mapped[i] = true;
-            mappings[i] = map.Map(mappings[i]);
-        }
-    }
+    mapper.AddMapping(line);
 }
 
-var closest = mappings.Min();
+mappers.Add(mapper);
+
+
 sw.Stop();
 
-Console.WriteLine($"Result: {mappings.Min()}");
-Console.WriteLine($"Elapsed: {sw.Elapsed.TotalMilliseconds}ms");
+Console.WriteLine($"Total time: {sw.ElapsedMilliseconds}ms");
 
-internal sealed class SeedMap(long Destination, long Start, long Range)
+internal record SeedRange(long Start, long Stop);
+
+internal sealed class SeedMapper
 {
-    public long Map(long source)
+    private List<SeedMapping> _mappings = new List<SeedMapping>();
+
+    public void AddMapping(string line)
     {
-        return Destination + (source - Start);
+        var parts = line.Trim().Split(" ").Select(long.Parse).ToArray();
+        var mapping = new SeedMapping(parts[0], parts[1], parts[2]);
+
+        _mappings.Add(mapping);
     }
     
-    public bool InRange(long source)
+    public SeedMapping[] Intersects(SeedRange range)
     {
-        return source >= Start && source < Start + Range;
+        return _mappings.Where(m => m.Source >= range.Start && m.Source <= range.Stop).ToArray();
     }
+
 }
+internal record SeedMapping(long Destination, long Source, long Range);
---
+++ 2023/2023-12-06/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var sw = new System.Diagnostics.Stopwatch();
+sw.Start();
+
+var lines = File.ReadAllLines(@"../../../Input/File1 .txt");
+
+var evaluator = new Evaluator(lines);
+var result = evaluator.Evaluate();
+
+sw.Stop();
+
+Console.WriteLine($"Result: {result}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
+internal record Evaluator(string[] lines)
+{
+    public int Evaluate()
+    {
+        var times = ParseLine(lines[0]);
+        var distances = ParseLine(lines[1]);
+        var combinations = new int[times.Length];
+
+        for (var loop = 0; loop < times.Length; loop++)
+            combinations[loop] = EvaluateRace(times[loop], distances[loop]);
+
+        return combinations.Aggregate(1, (a, b) => a * b);
+    }
+    
+    private int EvaluateRace(int time, int distance)
+    {
+        var combinations = 0;
+        
+        for (var loop = 0; loop < time; loop++)
+        {
+            var speed = loop;
+            var result = speed * (time - loop);
+
+            if (result > distance)
+                combinations++;
+        }
+
+        return combinations;
+    }
+
+    private int[] ParseLine(string values)
+    {
+        var sanitized = Regex.Replace(values.Trim(), @"\s+", " ");
+
+        return sanitized.Split(":")[1].Trim().Split(" ").Select(int.Parse).ToArray();
+    }
+}
