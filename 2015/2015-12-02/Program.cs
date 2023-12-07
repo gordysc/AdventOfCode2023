e18a63b11842db3ae2e42ff561ef70362482dbbc
Luke Gordon <gordysc@gmail.com>
2023-12-07 17:10:16 -0600

    2015-12-02: part 1

---
+++ 2015/2015-12-02/Program.cs
@@ -0,0 +1,26 @@
+﻿var sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
+
+var lines = File.ReadAllLines(@"../../../Input.txt");
+
+var area = 0;
+
+foreach (var line in lines)
+{
+    var values = line.Split('x').Select(int.Parse).ToArray();
+    var box = new Box(values[0], values[1], values[2]);
+
+    area += box.TotalArea + box.MinimumArea;
+}
+
+sw.Stop();
+
+Console.WriteLine($"Total area: {area}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
+
+record Box(int Height, int Width, int Length)
+{
+    public int TotalArea => 2 * (Height * Width + Width * Length + Length * Height);
+    public int MinimumArea => new[] { Height * Width, Width * Length, Length * Height }.Min();
+}
