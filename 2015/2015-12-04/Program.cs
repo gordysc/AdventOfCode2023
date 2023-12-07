295169e20c03be4436edf46e59d1ffcf5c5d8b02
Luke Gordon <gordysc@gmail.com>
2023-12-07 17:56:54 -0600

    2015-12-04: part 1

---
+++ 2015/2015-12-04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
+
+const string text = "ckczppom";
+var loop = 1L;
+
+do
+{
+    var hash = MD5.HashData(Encoding.ASCII.GetBytes($"{text}{loop}"));
+    var hex = BitConverter.ToString(hash).Replace("-", "");
+
+    if (hex.StartsWith("00000"))
+        break;
+
+    loop++;
+
+} while (true);
+
+sw.Stop();
+
+Console.WriteLine($"Answer: {text}{loop}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
