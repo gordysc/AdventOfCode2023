d3537c18170027d84111328b73b80d30da668f8a
Luke Gordon <gordysc@gmail.com>
2023-12-07 17:30:49 -0600

    2015-12-03: part 1

---
+++ 2015/2015-12-03/Program.cs
@@ -0,0 +1,33 @@
+﻿var sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
+
+var text = File.ReadAllText(@"../../../Input.txt");
+
+var evaluator = new Evaluator();
+var houses = evaluator.Evaluate(text);
+
+sw.Stop();
+
+Console.WriteLine($"Houses: {houses}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
+
+internal class Evaluator
+{
+    public int Evaluate(string text)
+    {
+        var cursor = (0, 0);
+        var points = new HashSet<(int, int)> { (0, 0) };
+
+        foreach (char c in text)
+        {
+            var x = cursor.Item1 + c switch { '>' => 1, '<' => -1, _ => 0 };
+            var y = cursor.Item2 + c switch { '^' => 1, 'v' => -1, _ => 0 };
+
+            points.Add((x, y));
+            cursor = (x, y);
+        }
+
+        return points.Count();
+    }
+}
