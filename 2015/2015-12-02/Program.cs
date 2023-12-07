ca2140c9f83bfc27d58203063b200280f42c746b
Luke Gordon <gordysc@gmail.com>
2023-12-07 17:12:32 -0600

    2015-12-02: part 2

--- 2015/2015-12-02/Program.cs
+++ 2015/2015-12-02/Program.cs
@@ -4,23 +4,23 @@ sw.Start();
 
 var lines = File.ReadAllLines(@"../../../Input.txt");
 
-var area = 0;
+var feet = 0;
 
 foreach (var line in lines)
 {
     var values = line.Split('x').Select(int.Parse).ToArray();
     var box = new Box(values[0], values[1], values[2]);
 
-    area += box.TotalArea + box.MinimumArea;
+    feet += box.Volume + box.Perimiter;
 }
 
 sw.Stop();
 
-Console.WriteLine($"Total area: {area}");
+Console.WriteLine($"Answer: {feet}");
 Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
 
 record Box(int Height, int Width, int Length)
 {
-    public int TotalArea => 2 * (Height * Width + Width * Length + Length * Height);
-    public int MinimumArea => new[] { Height * Width, Width * Length, Length * Height }.Min();
+    public int Volume => Height * Width * Length;
+    public int Perimiter => new[] { Height + Width, Width + Length, Length + Height }.Min() * 2;
 }
