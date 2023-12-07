58a6f150ad05e12b6122785a4ea3089586f5914c
Luke Gordon <gordysc@gmail.com>
2023-12-07 17:02:11 -0600

    2015-12-01: part 1

---
+++ 2015/2015-12-01/Program.cs
@@ -0,0 +1,16 @@
+﻿var sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
+
+var floor = 0;
+var text = File.ReadAllText(@"../../../Input.txt");
+
+foreach (char c in text)
+    floor += c == '(' ? 1 : -1;
+
+Console.WriteLine(text);
+
+sw.Stop();
+
+Console.WriteLine($"Floor: {floor}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
