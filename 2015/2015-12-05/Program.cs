8e34aa60dd559779ff319bd1e811d885f4f0a675
Luke Gordon <gordysc@gmail.com>
2023-12-08 08:59:14 -0600

    2015-12-05: part 2

--- 2015/2015-12-05/Program.cs
+++ 2015/2015-12-05/Program.cs
@@ -15,20 +15,7 @@ Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
 
 internal class Evaluator
 {
-    private static readonly char[] Vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
-    private static readonly string[] InvalidStrings = new string[] { "ab", "cd", "pq", "xy" };
-
     public bool IsValid(string line) =>
-        SatisfiesVowelRule(line) && 
-        SatisfiesDoubleLetterRule(line) && 
-        SatisfiesInvalidStringRule(line);
-    
-    private bool SatisfiesVowelRule(string line) =>
-        Vowels.Select(v => line.Count(x => x == v)).Sum() >= 3;
-
-    private bool SatisfiesDoubleLetterRule(string line) =>
-        Regex.IsMatch(line, @"(.)\1");
-    
-    private bool SatisfiesInvalidStringRule(string line) =>
-        !InvalidStrings.Any(line.Contains);
+        Regex.IsMatch(line, @"(..).*\1") &&
+        Regex.IsMatch(line, @"(.).\1");
 }
