35f50c14c40a2c44ba0a23db4107741b88794089
Luke Gordon <gordysc@gmail.com>
2023-12-07 18:42:12 -0600

    2015-12-05: part 1

---
+++ 2015/2015-12-05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+var sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
+
+var evaluator = new Evaluator();
+var lines = File.ReadAllLines(@"../../../Input.txt");
+var total = lines.Count(line => evaluator.IsValid(line));
+
+sw.Stop();
+
+Console.WriteLine($"Total: {total}");
+Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
+
+internal class Evaluator
+{
+    private static readonly char[] Vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+    private static readonly string[] InvalidStrings = new string[] { "ab", "cd", "pq", "xy" };
+
+    public bool IsValid(string line) =>
+        SatisfiesVowelRule(line) && 
+        SatisfiesDoubleLetterRule(line) && 
+        SatisfiesInvalidStringRule(line);
+    
+    private bool SatisfiesVowelRule(string line) =>
+        Vowels.Select(v => line.Count(x => x == v)).Sum() >= 3;
+
+    private bool SatisfiesDoubleLetterRule(string line) =>
+        Regex.IsMatch(line, @"(.)\1");
+    
+    private bool SatisfiesInvalidStringRule(string line) =>
+        !InvalidStrings.Any(line.Contains);
+}
