0c98026cd7d86051cf0b37c89f77db436a507d87
Luke Gordon <gordysc@gmail.com>
2023-12-12 10:57:40 -0600

    2023-12-12: part 2

--- 2023/2023-12-12/Program.cs
+++ 2023/2023-12-12/Program.cs
@@ -12,58 +12,83 @@ Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
 internal class Solution
 {
     private static readonly string[] Input = File.ReadAllLines(@"../../../Data/Input.txt");
-    private readonly Dictionary<int, string[]> _permutations = new();
+    private readonly Dictionary<string, long> cache = new();
+
     public void Solve()
     {
-        var total = 0;
+        var total = 0L;
 
-        foreach (var line in Input)
-            total += Arrangements(line);
-        
+        for (var loop = 0; loop < Input.Length; loop++)
+            total += CalculateArrangements(Input[loop]);
+
         Console.WriteLine($"Total: {total}");
     }
     
-    private int Arrangements(string line)
+    private long CalculateArrangements(string line)
     {
-        var (input, counts) = Parse(line);
+        var (data, numbers) = Parse(line);
 
-        var chars = input.ToCharArray();
-        var unknowns = Regex.Matches(input, @"[?]").Select(m => m.Index).ToArray();
-        var permutations = GeneratePermutations(chars, 0, unknowns).ToHashSet();
+        var input = string.Join("?", Enumerable.Repeat(data, 5));
+        var counts = Enumerable.Repeat(numbers, 5).SelectMany(n => n).ToArray();
 
-        var total = 0;
-        
-        foreach(var permutation in permutations)
-        {
-            var segments = permutation.Split(".", StringSplitOptions.RemoveEmptyEntries);
-            
-            if (segments.Length != counts.Length)
-                continue;
-            
-            if (Enumerable.Range(0, segments.Length).All(loop => segments[loop].Length == counts[loop]))
-                total++;
-        }
-        
-        return total;
+        var result = Calculate(input, counts);
+
+        // Console.WriteLine(string.Join("", Enumerable.Repeat("-", 20).ToArray()));
+        // Console.WriteLine($"Input: {input}");
+        // Console.WriteLine($"Counts: {string.Join(",", counts)}");
+        // Console.WriteLine($"Result: {result}");
+        // Console.WriteLine(string.Join("", Enumerable.Repeat("-", 20).ToArray()));
+
+        return result;
     }
 
-    private static string[] GeneratePermutations(char[] chars, int index, int[] unknowns)
+    private long Calculate(string input, int[] counts)
     {
-        if (index == chars.Length)
-            return new string[] { new string(chars) };
-        
-        if (!unknowns.Contains(index))
-            return GeneratePermutations(chars, index + 1, unknowns);
+        var key = $"[{input}|{string.Join(":", counts)}";
 
-        var copy = (char[])chars.Clone();
-        
-        copy[index] = '.';
-        var operational = GeneratePermutations(copy, index + 1, unknowns);
+        if (cache.TryGetValue(key, out var result))
+            return result;
 
-        copy[index] = '#';
-        var damaged = GeneratePermutations(copy, index + 1, unknowns);
-        
-        return operational.Concat(damaged).ToArray();
+        result = CalculateInternal(input, counts);
+
+        cache.Add(key, result);
+
+        return result;
+    }
+
+    private long CalculateInternal(string input, int[] counts)
+    {
+        // Check if there are still damaged springs
+        if (counts.Length == 0)
+            return input.Contains('#') ? 0L : 1L;
+
+        // No matches if there are more count blocks than characters
+        if (input.Length < counts.Length)
+            return 0L;
+
+        // No matches if the input length is less than the first count
+        if (input.Length < counts[0])
+            return 0L;
+
+        // Skip all the operational springs and continue calculating
+        if (input.StartsWith("."))
+            return Calculate(input.TrimStart('.'), counts);
+
+        // Calculate both possibilities
+        if (input.StartsWith("?"))
+            return Calculate('#' + input[1..], counts) + Calculate('.' + input[1..], counts);
+
+        // No matches if the first block length is less than the first count
+        if (input.Contains('.') && input.IndexOf('.') + 1 <= counts[0])
+            return 0L;
+
+        // No matches if there isn't a potential . after the first damaged spring block
+        if (input.Length > counts[0] && input[counts[0]] == '#')
+            return 0L;
+
+        var skip = input.Length == counts[0] ? counts[0] : counts[0] + 1;
+
+        return Calculate(input[skip..], counts[1..]);
     }
 
     private static (string, int[]) Parse(string line)
