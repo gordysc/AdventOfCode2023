cae19e10e2fb1f8f6669ea5240b1852cb1cc18f0
Luke Gordon <gordysc@gmail.com>
2023-12-14 06:41:10 -0600

    chore(2023-12-14): save work

--- 2023/2023-12-14/Program.cs
+++ 2023/2023-12-14/Program.cs
@@ -12,35 +12,76 @@ Console.WriteLine($"Total Time: {sw.Elapsed.TotalMilliseconds}ms");
 
 internal class Solution
 {
-    private static readonly string[] Lines = File.ReadAllLines(@"../../../Data/Input.txt");
-    private static readonly char[][] Grid = Lines.Select(l => l.ToCharArray()).ToArray();
-    
-    private static readonly int Height = Grid.Length;
-    private static readonly int Width = Grid[0].Length;
+    private static readonly string[] Lines = File.ReadAllLines(@"../../../Data/Example.txt");
+    private static long Rotations = 1_000_000_000L;
 
     public void Solve()
     {
-        var result = TiltNorth();
+        var tilted = (string[]) Lines.Clone();
+
+        for (var loop = 0; loop < 3; loop++)
+            tilted = Cycle(tilted);
         
-        Console.WriteLine($"Result: {result}");
+        foreach (var line in tilted)
+            Console.WriteLine(line);
+        // var rotated = RotateRight(grid).Select(row => new string(row)).ToArray();
+        // var result = CalculateLoad(rotated);
+        //
+        // Console.WriteLine();
+        // Console.WriteLine($"Result: {result}");
     }
 
-    private static int TiltNorth()
+    private static string[] Cycle(string[] lines) =>
+        TiltEast(TiltSouth(TiltWest(TiltNorth(lines))));
+
+    private static string[] TiltNorth(string[] lines)
     {
-        var rotated = Enumerable.Range(0, Height)
-            .Select(x => string.Join("", Grid.Select(row => row[x])))
-            .Select(l => string.Join("", l.ToCharArray().Reverse()))
-            .ToArray();
-        
-        return CalculateLoad(rotated);
+        var grid = lines.Select(line => line.ToCharArray()).ToArray();
+        var rotated = RotateRight(grid).Select(row => new string(row)).ToArray();
+        var tilted = rotated.Select(Tilt).Select(line => line.ToCharArray()).ToArray();
+
+        return RotateLeft(tilted).Select(row => new string(row)).ToArray();
     }
+
+    private static string[] TiltEast(string[] lines) =>
+        lines.Select(Tilt).ToArray();
+
+    private static string[] TiltSouth(string[] lines)
+    {
+        var grid = lines.Select(line => line.ToCharArray()).ToArray();
+        var rotated = RotateLeft(grid).Select(row => new string(row)).ToArray();
+        var tilted = rotated.Select(Tilt).Select(line => line.ToCharArray()).ToArray();
+
+        return RotateRight(tilted).Select(row => new string(row)).ToArray();
+    }
+    
+    private static string[] TiltWest(string[] lines)
+    {
+        var reversed = lines.Select(line => new string(line.Reverse().ToArray())).ToArray();
+        
+        return reversed.Select(Tilt).Select(line => new string(line.Reverse().ToArray())).ToArray();
+    }
+
+    private static char[][] RotateRight(char[][] grid) =>
+        Enumerable.Range(0, grid.Length)
+            .Select(x => grid.Select(row => row[x]))
+            .Select(row => row.Reverse().ToArray())
+            .ToArray();
+    
+    private static char[][] RotateLeft(char[][] grid) =>
+        Enumerable.Range(0, grid.Length)
+            .Select(x => grid.Select(row => row[grid.Length - x - 1]).ToArray())
+            .ToArray();
+
+    private static char[][] FlipHorizontal(char[][] grid) =>
+        grid.Reverse().ToArray();
 
     private static int CalculateLoad(string[] lines) =>
         lines.Select(Tilt).Select(CalculateRowLoad).Sum();
 
     private static int CalculateRowLoad(string line) =>
         Regex.Matches(line, "[O]").Select(m => m.Index + 1).ToArray().Sum();
-
+    
     private static string Tilt(string line) =>
         string.Join("#", line.Split("#").Select(ShiftRight).ToArray());
 
