cc74ba1640495d4c2c96b151cf4ce317e7b6486f
Luke Gordon <gordysc@gmail.com>
2023-12-19 14:12:53 -0600

    chore(2023-12-19): saving work

--- 2023/2023-12-19/Program.cs
+++ 2023/2023-12-19/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
-var input = File.ReadAllText(@"../../../Data/Input.txt");
+var input = File.ReadAllText(@"../../../Data/Example.txt");
 // var input = File.ReadAllLines(@"../../../Data/Input.txt");
 var sw = new System.Diagnostics.Stopwatch();
 var solution = new Solution();
@@ -15,58 +15,87 @@ internal class Solution
 {
     private static readonly string EOL = Environment.NewLine;
     private static readonly string[] SectionSeparator = new[] { EOL + EOL };
+
+    private static readonly string[] Letters = { "x", "m", "a", "s" };
+    private static readonly int MaxValue = 4000;
     public void Solve(string input)
     {
         var sections = input.Split(SectionSeparator, StringSplitOptions.RemoveEmptyEntries);
-
         var workflows = ParseWorkflows(sections[0].Split(EOL));
-        var ratings = ParseRatings(sections[1].Split(EOL));
 
-        var accepted = ratings.Where(r => Evaluate(r, workflows, "in"));
+        var tracker = Letters.Select(l => (l, (1, MaxValue))).ToDictionary();
+        var results = Evaluate(tracker, workflows, "in");
 
-        var answer = accepted.Aggregate(0, (acc, rating) => acc + rating.Values.Sum());
+        var answer = results.Aggregate(0L, (acc, r) => acc + ComputeCombinations(r));
         
         Console.WriteLine($"Answer: {answer}");
     }
 
-    private static bool Evaluate(Dictionary<string, int> rating, Dictionary<string, string> workflows, string label)
+    private static long ComputeCombinations(Dictionary<string, (int, int)> tracker)
+    {
+        var result = 1L;
+
+        foreach (var key in tracker.Keys)
+        {
+            var (min, max) = tracker[key];
+            result *= (max - min + 1);
+        }
+
+        return result;
+    }
+
+    private static Dictionary<string, (int, int)>[] Evaluate(Dictionary<string, (int, int)> previous, Dictionary<string, string> workflows, string label)
     {
         var workflow = workflows[label];
         var operations = workflow.Split(",");
+        var tracker = new Dictionary<string, (int, int)>(previous);
+        var none = Array.Empty<Dictionary<string, (int, int)>>();
 
-        var operation = operations.FirstOrDefault(op =>
-            op == "A" || op == "R" || (op.Contains('<') || op.Contains('>')) && LessOrGreaterThan(rating, op)
-        ) ?? operations.Last();
-
-        if (operation == "A") return true;
-        if (operation == "R") return false;
-
-        if (operation.Contains('>') || operation.Contains('<'))
+        return operations.SelectMany(operation =>
         {
-            var output = operation.Split(":").Last();
-            return output == "A" || (output != "R" && Evaluate(rating, workflows, output));
-        }
+            // See if we reached the end
+            if (operation == "A") return [previous];
+            if (operation == "R") return none;
 
-        return Evaluate(rating, workflows, operation);
+            var parts = operation.Split(new[] { ">", "<", ":" }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (operation.Contains('>') || operation.Contains('<'))
+            {
+                var op = operation.Contains('>') ? "gt" : "lt";
+
+                var key = parts[0];
+                var (min, max) = tracker[key];
+                // 98548139872000
+                // 167409079868000
+                var value = int.Parse(parts[1]);
+                // If we require a value greater than our letter's max value abort
+                if (op == "gt" && value > max) return none;
+
+                // Raise the minimum range value (if applicable)
+                if (op == "gt") tracker[key] = (Math.Max(min, value + 1), max);
+
+                // If we require a value less than our letter's min value abort
+                if (op == "lt" && value < min) return none;
+
+                // Lower the max range value (if applicable)
+                if (op == "lt") tracker[key] = (min, Math.Min(max, value - 1));
+
+                // If our min value > max value this is an invalid path, abort
+                if (tracker[key].Item1 > tracker[key].Item2) return none;
+
+                var destination = parts[2];
+
+                // Check if we are done...
+                if (destination == "A") return [tracker];
+                if (destination == "R") return none;
+
+                // Get all the possible outcomes from the next jump
+                return Evaluate(tracker, workflows, destination);
+            }
+
+            return Evaluate(tracker, workflows, operation);
+        }).Where(r => r.Keys.Count > 0).ToArray();
     }
-
-    private static bool LessOrGreaterThan(Dictionary<string, int> rating, string operation)
-    {
-        var parts = operation.Split(new[] { "<", ">", ":" }, StringSplitOptions.RemoveEmptyEntries);
-
-        if (operation.Contains(">"))
-            return rating[parts[0]] > int.Parse(parts[1]);
-
-        return rating[parts[0]] < int.Parse(parts[1]);
-    }
-
-    private static IEnumerable<Dictionary<string, int>> ParseRatings(IEnumerable<string> lines) =>
-        lines.Select(line =>
-            line.Replace("{", "").Replace("}", "")
-                .Split(',')
-                .Select(pair => pair.Split('='))
-                .ToDictionary(items => items[0], items => int.Parse(items[1]))
-        ).ToArray();
 
     private static Dictionary<string, string> ParseWorkflows(IEnumerable<string> lines) =>
         lines.ToDictionary(
